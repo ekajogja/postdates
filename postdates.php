a5643ee185a00dd409196ff900c1f7917a298cce
ekajogja <ekajogja@gmail.com>
2025-08-12 22:27:42 +0700

    feat: Update plugin with new features and security enhancements
    This commit introduces a major update to the PostDates plugin, version 2.0.
    
    The following improvements have been made:
    - Added a new feature that allows you to set a custom date format.
    - Implemented internationalization (I18N) to make the plugin translatable.
    - Refactored the codebase for better readability, maintainability, and performance.
    - Enhanced security with more specific and robust input sanitization.
    - Fixed a bug in the date display logic for different post types.
    - Updated the readme.txt file with a detailed changelog and documentation for the new feature.

--- postdates.php
+++ postdates.php
@@ -2,11 +2,13 @@
 /*
 Plugin Name: PostDates
 Plugin URI: https://github.com/ekajogja/postdates
-Description: A simple WordPress plugin that displays the publication and last update date on posts, pages, and custom post types.
-Version: 1.0
+Description: A simple WordPress plugin that displays the publication and last update date on posts, pages, and custom post types. Now with date format and position options.
+Version: 2.0
 Author: ekajogja
 Author URI: https://github.com/ekajogja
 License: GPL2
+Text Domain: postdates
+Domain Path: /languages
 */
 
 if (!defined('ABSPATH')) {
@@ -19,16 +21,22 @@ class PostDates
 
     public function __construct()
     {
+        add_action('plugins_loaded', array($this, 'load_textdomain'));
         add_action('admin_menu', array($this, 'add_plugin_page'));
         add_action('admin_init', array($this, 'register_settings'));
         add_action('the_content', array($this, 'display_dates'));
     }
 
+    public function load_textdomain()
+    {
+        load_plugin_textdomain('postdates', false, dirname(plugin_basename(__FILE__)) . '/languages/');
+    }
+
     public function add_plugin_page()
     {
         add_options_page(
-            'PostDates',
-            'PostDates',
+            __('PostDates Settings', 'postdates'),
+            __('PostDates', 'postdates'),
             'manage_options',
             'postdates',
             array($this, 'create_admin_page')
@@ -40,7 +48,7 @@ class PostDates
         $this->options = get_option('postdates_options');
         ?>
         <div class="wrap">
-            <h1>PostDates</h1>
+            <h1><?php _e('PostDates Settings', 'postdates'); ?></h1>
             <form method="post" action="options.php">
                 <?php
                 settings_fields('postdates_options_group');
@@ -67,18 +75,38 @@ class PostDates
             'postdates'
         );
 
-        $this->add_settings_field('show_both_on_posts', 'Show both published and last update on Posts');
-        $this->add_settings_field('show_both_on_pages', 'Show both published and last update on Pages');
-        $this->add_settings_field('show_both_on_custom_posts', 'Show both published and last update on Custom Posts');
-        $this->add_settings_field('date_position', 'Position of Dates');
-    }
+        $checkbox_settings = array(
+            'show_both_on_posts' => __('Show both published and last update on Posts', 'postdates'),
+            'show_both_on_pages' => __('Show both published and last update on Pages', 'postdates'),
+            'show_both_on_custom_posts' => __('Show both published and last update on Custom Posts', 'postdates'),
+        );
 
-    private function add_settings_field($id, $title)
-    {
+        foreach ($checkbox_settings as $id => $title) {
+            add_settings_field(
+                $id,
+                $title,
+                array($this, 'checkbox_callback'),
+                'postdates',
+                'postdates_options_section',
+                array(
+                    'id' => $id,
+                    'label_for' => $id,
+                )
+            );
+        }
+
         add_settings_field(
-            $id,
-            $title,
-            array($this, $id . '_callback'),
+            'date_position',
+            __('Position of Dates', 'postdates'),
+            array($this, 'date_position_callback'),
+            'postdates',
+            'postdates_options_section'
+        );
+
+        add_settings_field(
+            'date_format',
+            __('Date Format', 'postdates'),
+            array($this, 'date_format_callback'),
             'postdates',
             'postdates_options_section'
         );
@@ -86,34 +114,37 @@ class PostDates
 
     public function sanitize($input)
     {
-        $sanitary_values = array();
-        foreach ($input as $key => $val) {
-            $sanitary_values[$key] = sanitize_text_field($val);
+        $new_input = array();
+
+        if (isset($input['show_both_on_posts'])) {
+            $new_input['show_both_on_posts'] = '1';
         }
-        return $sanitary_values;
+        if (isset($input['show_both_on_pages'])) {
+            $new_input['show_both_on_pages'] = '1';
+        }
+        if (isset($input['show_both_on_custom_posts'])) {
+            $new_input['show_both_on_custom_posts'] = '1';
+        }
+
+        if (isset($input['date_position']) && in_array($input['date_position'], array('above', 'below'), true)) {
+            $new_input['date_position'] = $input['date_position'];
+        }
+
+        if (isset($input['date_format'])) {
+            $new_input['date_format'] = sanitize_text_field($input['date_format']);
+        }
+
+        return $new_input;
     }
 
-    public function show_both_on_posts_callback()
+    public function checkbox_callback($args)
     {
+        $id = $args['id'];
         printf(
-            '<input type="checkbox" name="postdates_options[show_both_on_posts]" %s>',
-            isset($this->options['show_both_on_posts']) ? 'checked' : ''
-        );
-    }
-
-    public function show_both_on_pages_callback()
-    {
-        printf(
-            '<input type="checkbox" name="postdates_options[show_both_on_pages]" %s>',
-            isset($this->options['show_both_on_pages']) ? 'checked' : ''
-        );
-    }
-
-    public function show_both_on_custom_posts_callback()
-    {
-        printf(
-            '<input type="checkbox" name="postdates_options[show_both_on_custom_posts]" %s>',
-            isset($this->options['show_both_on_custom_posts']) ? 'checked' : ''
+            '<input type="checkbox" id="%s" name="postdates_options[%s]" value="1" %s/>',
+            esc_attr($id),
+            esc_attr($id),
+            checked(isset($this->options[$id]), true, false)
         );
     }
 
@@ -122,51 +153,66 @@ class PostDates
         $position = isset($this->options['date_position']) ? $this->options['date_position'] : 'above';
         ?>
         <select name="postdates_options[date_position]">
-            <option value="above" <?php selected($position, 'above'); ?>>Above Content</option>
-            <option value="below" <?php selected($position, 'below'); ?>>Below Content</option>
+            <option value="above" <?php selected($position, 'above'); ?>><?php esc_html_e('Above Content', 'postdates'); ?></option>
+            <option value="below" <?php selected($position, 'below'); ?>><?php esc_html_e('Below Content', 'postdates'); ?></option>
         </select>
         <?php
     }
 
+    public function date_format_callback()
+    {
+        $date_format = isset($this->options['date_format']) ? $this->options['date_format'] : get_option('date_format');
+        printf(
+            '<input type="text" id="date_format" name="postdates_options[date_format]" value="%s" />',
+            esc_attr($date_format)
+        );
+        echo '<p class="description">' . sprintf(
+            /* translators: %s: link to WordPress date formatting documentation */
+            __('Enter a valid PHP date format. See the <a href="%s" target="_blank">WordPress documentation</a> for more information.', 'postdates'),
+            'https://wordpress.org/support/article/formatting-date-and-time/'
+        ) . '</p>';
+    }
+
     public function display_dates($content)
     {
-        if (is_singular()) {
-            $this->options = get_option('postdates_options');
-            $post_type = get_post_type();
-            $published_date = get_the_date();
-            $updated_date = get_the_modified_date();
+        if (!is_singular()) {
+            return $content;
+        }
 
-            $display_text = '';
+        $this->options = get_option('postdates_options');
+        $post_type = get_post_type();
 
-            if ($post_type == 'post' && isset($this->options['show_both_on_posts'])) {
-                $display_text .= sprintf(
-                    'Published: %1$s | Updated: %2$s',
-                    esc_html($published_date),
-                    esc_html($updated_date)
-                );
-            } elseif ($post_type == 'page' && isset($this->options['show_both_on_pages'])) {
-                $display_text .= sprintf(
-                    'Published: %1$s | Updated: %2$s',
-                    esc_html($published_date),
-                    esc_html($updated_date)
-                );
-            } elseif (isset($this->options['show_both_on_custom_posts'])) {
-                $display_text .= sprintf(
-                    'Published: %1$s | Updated: %2$s',
-                    esc_html($published_date),
-                    esc_html($updated_date)
-                );
-            }
+        $option_name = '';
+        if ($post_type === 'post') {
+            $option_name = 'show_both_on_posts';
+        } elseif ($post_type === 'page') {
+            $option_name = 'show_both_on_pages';
+        } else {
+            $option_name = 'show_both_on_custom_posts';
+        }
 
-            if (!empty($display_text)) {
-                $position = isset($this->options['date_position']) ? $this->options['date_position'] : 'above';
-                if ($position == 'above') {
-                    $content = '<p>' . $display_text . '</p>' . $content;
-                } else {
-                    $content .= '<p>' . $display_text . '</p>';
-                }
+        if (isset($this->options[$option_name])) {
+            $date_format = isset($this->options['date_format']) ? $this->options['date_format'] : get_option('date_format');
+            $published_date = get_the_date($date_format);
+            $updated_date = get_the_modified_date($date_format);
+
+            $display_text = sprintf(
+                /* translators: 1: Published date, 2: Updated date */
+                esc_html__('Published: %1$s | Updated: %2$s', 'postdates'),
+                esc_html($published_date),
+                esc_html($updated_date)
+            );
+
+            $position = isset($this->options['date_position']) ? $this->options['date_position'] : 'above';
+            $date_html = '<p>' . $display_text . '</p>';
+
+            if ($position === 'above') {
+                $content = $date_html . $content;
+            } else {
+                $content .= $date_html;
             }
         }
+
         return $content;
     }
 }
--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: ekajogja
 Donate link: https://paypal.me/ekajogja
 Tags: date, last-updated, published-date
 Requires at least: 4.7
-Tested up to: 6.6.1
-Stable tag: 1.0
+Tested up to: 6.7
+Stable tag: 2.0
 Requires PHP: 7.0
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
@@ -13,7 +13,7 @@ Display the publication and last update dates on your posts, pages, and custom p
 
 == Description ==
 
-PostDates allows you to easily display the publication and last update dates for your content. It includes a settings page where you can configure which dates to show and their position.
+PostDates allows you to easily display the publication and last update dates for your content. It includes a settings page where you can configure which dates to show, their position, and the date format.
 
 == Installation ==
 
@@ -27,6 +27,10 @@ PostDates allows you to easily display the publication and last update dates for
 
 Go to the plugin settings page and select the desired position (above or below the content).
 
+= How do I change the format of the dates? =
+
+Go to the plugin settings page and enter a valid PHP date format in the "Date Format" field.
+
 == Screenshots ==
 
 1. Plugin setting
@@ -39,6 +43,13 @@ Go to the plugin settings page and select the desired position (above or below t
 
 == Changelog ==
 
+= 2.0 =
+* Added a date format option to the settings page.
+* Implemented internationalization (I18N) for translation readiness.
+* Refactored the code for better performance and maintainability.
+* Improved security with more specific input sanitization.
+* Fixed a bug in date display logic for different post types.
+
 = 1.0 =
 * Initial release of the PostDates plugin.
 * Features include displaying publication and last update dates on posts, pages, and custom post types.
@@ -46,6 +57,9 @@ Go to the plugin settings page and select the desired position (above or below t
 * Added option to choose the position of dates (above or below the content).
 
 == Upgrade Notice ==
+
+= 2.0 =
+This version introduces a new date format option and includes significant code improvements. Your settings should be preserved, but it's always a good idea to check them after updating.
 
 = 1.0 =
 None.
